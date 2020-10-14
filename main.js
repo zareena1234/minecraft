acd1aeafcbce1359241b2715b23217246b78c0a2
zareena1234 <72873824+zareena1234@users.noreply.github.com>
2020-10-14 19:44:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,17 @@
-noseX=0;
-noseY=0;
+Webcam.set({
+width:350, height:300, image_format:'png',png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
 
-function preload() {
-  clown_nose = loadImage('https://i.postimg.cc/NGyGnHzY/l1.png');
-}
-
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.size(300, 300);
-  video.hide();
-
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
-
-function modelLoaded() {
-  console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
+function take_snapshot(){
+Webcam.snap(function(data_uri)
 {
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x-25;
-    noseY = results[0].pose.nose.y+15;
-  }
+document.getElementById("result").innerHTML='<img id="capture_image"src="'+data_uri+'"/>';
+}
+);
 }
 
-function draw() {
-  image(video, 0, 0, 300, 300);
-  image(clown_nose, noseX, noseY, 50, 20);
-}
 
-function take_snapshot(){    
-  save('myFilterImage.png');
-}
+
+
